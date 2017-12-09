f779a13d3e43ab74f48d5de74de3505ed1b455d9
Edenilson J dos Passos <edenilson.passos@yahoo.com>
2017-12-09 01:13:39 +0000

    composer

--- includes/db.php
+++ includes/db.php
@@ -4,7 +4,7 @@
     $user = "nicoede";                           //Your Cloud 9 username
     $pass = "";                                  //Remember, there is NO password by default!
     $db = "cms";                                  //Your database name you want to connect to
-    $port = 8889;                                //The port #. It is always 3306
+    $port = 3306;                                //The port #. It is always 3306
     
-    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die();
+    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysqli_error($connection));
 ?>
