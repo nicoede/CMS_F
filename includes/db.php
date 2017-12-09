dbd137495fb2e28c4ef23d7efe8173d29a9e57b1
Edenilson J dos Passos <edenilson.passos@yahoo.com>
2017-12-09 02:52:49 +0000

    cleardb config

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,6 @@
-<?php 
+
+<?php
+/* 
     //Connect to the database
     $host = "127.0.0.1";
     $user = "nicoede";                           //Your Cloud 9 username
@@ -7,4 +9,17 @@
     $port = 3306;                                //The port #. It is always 3306
     
     $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysqli_error($connection));
+*/
+?>
+
+
+<?php
+$url = parse_url(getenv("mysql://b8e6b3c540d809:7afb81de@us-cdbr-iron-east-05.cleardb.net/heroku_481ed05723dc72a?reconnect=true"));
+
+$server = $url["us-cdbr-iron-east-05.cleardb.net"];
+$username = $url["b8e6b3c540d809"];
+$password = $url["7afb81de"];
+$db = substr($url["heroku_481ed05723dc72a"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 ?>
